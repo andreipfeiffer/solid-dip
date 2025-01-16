acbaefe76bad8401a61c47f1a15e9efcb922a9c5
Andrei Pfeiffer <andrei@upsidedown.ro>
2025-01-16 22:50:38 +0200

    Add styling

--- src/CartPage.tsx
+++ src/CartPage.tsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 
 import { getCart } from "./api";
 import { useCartStore } from "./store";
+import "./CartPage.css";
 
 export function CartPage(props: { onLogout: () => void }) {
   const store = useCartStore();
@@ -13,27 +14,40 @@ export function CartPage(props: { onLogout: () => void }) {
     })();
   }, []);
 
+  const cartItems = Object.values(store.items);
+
   return (
     <div>
-      <button onClick={addNewItemToCart}>Add to cart</button>
-      <button onClick={props.onLogout}>Logout</button>
+      <nav className="nav">
+        <button onClick={addNewItemToCart}>Add to cart</button>
+        <button onClick={props.onLogout}>Logout</button>
+      </nav>
 
-      <ul>
-        {Object.values(store.items).map((item) => (
-          <li key={item.id}>
-            {item.quantity} x {item.name}
-            {item.quantity > 0 ? (
-              <button
-                onClick={() => store.updateQuantity(item.id, item.quantity + 1)}
-              >
-                +
-              </button>
-            ) : (
-              <em>Out of stock</em>
-            )}
-          </li>
-        ))}
-      </ul>
+      {cartItems.length > 0 ? (
+        <ul className="cart">
+          {cartItems.map((item) => (
+            <li key={item.id}>
+              <span>{item.name}</span>
+              <span>&times; {item.quantity}</span>
+              {item.quantity > 0 ? (
+                <button
+                  onClick={() =>
+                    store.updateQuantity(item.id, item.quantity + 1)
+                  }
+                >
+                  +
+                </button>
+              ) : (
+                <em className="empty">Out of stock</em>
+              )}
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <div className="empty_cart">
+          <em className="empty">No items in cart</em>
+        </div>
+      )}
 
       <strong>TOTAL: {store.getTotal()}</strong>
     </div>
